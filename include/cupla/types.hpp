d01e4513a826365c10b588455eedd97fa44ec1db
Jeffrey Kelling <j.kelling@hzdr.de>
2019-12-20 16:27:05 +0100

    Add use QueueOmp4*

--- include/cupla/types.hpp
+++ include/cupla/types.hpp
@@ -63,8 +63,7 @@ inline namespace CUPLA_ACCELERATOR_NAMESPACE
     defined(ALPAKA_ACC_CPU_B_SEQ_T_THREADS_ENABLED) ||                         \
     defined(ALPAKA_ACC_CPU_B_OMP2_T_SEQ_ENABLED) ||                            \
     defined(ALPAKA_ACC_CPU_B_SEQ_T_SEQ_ENABLED) ||                             \
-    defined(ALPAKA_ACC_CPU_B_TBB_T_SEQ_ENABLED) ||                             \
-    defined(ALPAKA_ACC_CPU_BT_OMP4_ENABLED)
+    defined(ALPAKA_ACC_CPU_B_TBB_T_SEQ_ENABLED)
 
     using AccDev = ::alpaka::dev::DevCpu;
 #   if (CUPLA_STREAM_ASYNC_ENABLED == 1)
@@ -129,15 +128,20 @@ inline namespace CUPLA_ACCELERATOR_NAMESPACE
     #endif
 #endif
 
+#endif
+
 #ifdef ALPAKA_ACC_CPU_BT_OMP4_ENABLED
+    using AccDev = ::alpaka::dev::DevOmp4;
+#   if (CUPLA_STREAM_ASYNC_ENABLED == 1)
+        using AccStream = ::alpaka::queue::QueueOmp4NonBlocking;
+#   else
+        using AccStream = ::alpaka::queue::QueueOmp4Blocking;
+#   endif
     using Acc = ::alpaka::acc::AccCpuOmp4<
         KernelDim,
         IdxType
     >;
 #endif
-
-#endif
-
 
 #ifdef ALPAKA_ACC_GPU_CUDA_ENABLED
     using AccDev = ::alpaka::dev::DevCudaRt;
